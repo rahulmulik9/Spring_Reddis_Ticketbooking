3ef6a18510bbb780565da8eec026239d722d38cf
Rahul Mulik <rahulmulik9@gmail.com>
2026-09-20 23:33:34 +0530

    Added auth dto

---
+++ Phase-2-jwt-auth/ticketbooking/src/main/java/com/rahul/ticketbooking/dto/AuthResponse.java
@@ -0,0 +1,11 @@
+package com.rahul.ticketbooking.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AuthResponse {
+
+    private String token;
+}
