ae1a36d66f6785430cb32a39a8212c3a4a908440
Rahul Mulik <rahulmulik9@gmail.com>
2026-09-21 09:04:02 +0530

    Added event repo

---
+++ Phase-3-Entity-CRUD/ticketbooking/src/main/java/com/rahul/ticketbooking/repository/EventRepository.java
@@ -0,0 +1,7 @@
+package com.rahul.ticketbooking.repository;
+
+import com.rahul.ticketbooking.entity.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<Event, Long> {
+}
